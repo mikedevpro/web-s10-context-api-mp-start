86fcfd3d9b34647c853dfd397b26f6d32b19c780
Michael Nobles <Mnobles33@gmail.com>
2024-12-21 06:28:16 -0700

    First commit

--- frontend/components/Quotes.js
+++ frontend/components/Quotes.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { QuotesContext } from '../context/quotesContext'
 
 export default function Quotes(props) {
+  const data = useContext(QuotesContext)
+  console.log(data)
   const {
     quotes,
     highlightedQuote,
--- frontend/context/quotesContext.js
+++ frontend/context/quotesContext.js
@@ -1,1 +1,17 @@
+import React, { createContext } from 'react'
 // Create your context here
+
+export const QuotesContext = createContext()
+
+const value = {
+    car: 'Honda Accord'
+}
+
+export function QuoteProvider(props) {
+
+    return (
+        <QuotesContext.Provider value={value}>
+            {props.children}
+        </QuotesContext.Provider>
+    )
+}
